bfc480ff9c28784b7f03a860c4bab0e778b467f2
Josephine-CM <kimmyjjmueni@gmail.com>
2022-03-09 16:17:03 +0300

    expansion of cards

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-const panels = document.querySelectorAll('.panel')
-console.log(panels[0])
+const panels =document.querySelectorAll('.panel')
+panels.forEach(panel =>{
+	panel.addEventListener('click',()=>{
+		removeActiveClass()
+		panel.classList.add('active')
+	})
+})
+function removeActiveClass(){
+	panels.forEach(panel =>{
+		panel.classList.remove('active')
+	})
+
+}
